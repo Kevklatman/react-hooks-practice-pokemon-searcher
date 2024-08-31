601ef60dfec4cd66d6c961b6bd15008e9822e3fe
Kevklatman <116696816+Kevklatman@users.noreply.github.com>
2024-08-30 20:34:37 -0600

    knj

--- src/components/Team.js
+++ src/components/Team.js
@@ -10,14 +10,25 @@ function Team() {
 
   return (
     <div className="Team">
-      <h1>My Team</h1>
-      {team.map((pokemon) => (
-        <div key={pokemon.id}>
-          <img src={pokemon.sprites.front} alt={pokemon.name} />
-          <div>{pokemon.name}</div>
-          <button onClick={() => handleRemove(pokemon.id)}>Remove</button>
-        </div>
-      ))}
+      <h1 className="Team-title">My Team</h1>
+      <div className="Team-pokemon-list">
+        {team.map((pokemon) => (
+          <div key={pokemon.id} className="Team-pokemon-card">
+            <img
+              src={pokemon.sprites.front}
+              alt={pokemon.name}
+              className="Team-pokemon-image"
+            />
+            <div className="Team-pokemon-name">{pokemon.name}</div>
+            <button
+              className="Team-remove-button"
+              onClick={() => handleRemove(pokemon.id)}
+            >
+              Remove
+            </button>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
